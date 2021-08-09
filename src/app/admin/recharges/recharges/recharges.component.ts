0343cb51d9cecf0c0517b5fb542ef1e45f8be0cf
Javier Monzon <javiermd13@gmail.com>
2021-08-08 19:37:29 -0500

    recargas y pago

--- src/app/admin/recharges/recharges/recharges.component.ts
+++ src/app/admin/recharges/recharges/recharges.component.ts
@@ -69,86 +69,69 @@ export class RechargesComponent implements OnInit {
     popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto')!;
     popupWin.document.open();
     popupWin.document.write(`
-<html>
-<head>
-    <title>Ticket</title>
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-        .company {
-          text-align: center;
-          margin-bottom: 9px;
-       }
-        .company img {
-          height: 50px;
-       }
-        .flex-container {
-          display: flex;
-          flex-direction: row;
-       }
-        @media (max-width: 800px) {
-          .flex-container {
-            flex-direction: column;
-         }
-       }
-        .ticket {
-          line-height: 1;
-          max-width: 180px;
-          width: 180px;
-       }
-        .ticket-company {
-          margin: 0 0 8px 0;
-       }
-        .msg-1 {
-          font-size: 10px;
-          font-weight: 600;
-          text-align: center;
-          margin-bottom: 3px;
-       }
-        .msg-2 {
-          font-size: 9px;
-          text-align: center;
-          line-height: 1;
-          margin-bottom: 2px;
-       }
-        .msg-3 {
-          margin: 10px 0 4px 0;
-          font-size: 9px;
-          font-weight: 600;
-          text-align: center;
-       }
-        .msg-4 {
-          font-size: 9px;
-          text-align: justify;
-       }
-        pre {
-          font-size: 9px;
-       }
-        textarea {
-          font-size: 12px;
-          text-align: justify;
-          line-height: 1;
-       }
-        img {
-          max-width: inherit;
-          width: inherit;
-       }
-        .example-card {
-          max-width: 400px;
-       }
-        .example-form {
-          min-width: 150px;
-          max-width: 500px;
-          width: 100%;
-       }
-        .example-full-width {
-          width: 100%;
-       }       
-    </style>
-</head>
-<body onload="window.print();window.close()"><div class="ticket">${printContents}</div></body>
-</html>`
+<html><head><title>Ticket</title><style>
+.ticket {
+  line-height: 1;
+  max-width: 180px;
+  width: 180px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.ticket pre {
+  font-size: 9px;
+}
+.ticket .ticket-top {
+  text-align: center;
+}
+.ticket .ticket-top img {
+  height: 50px;
+  margin-bottom: 9px;
+}
+.ticket .ticket-section {
+  margin: 11px 0 7px 0;
+}
+.ticket table.voucher {
+  font-size: 9px;
+  word-break: break-all;
+  white-space: pre-wrap;
+  white-space: -moz-pre-wrap;
+  white-space: -pre-wrap;
+  white-space: -o-pre-wrap;
+  word-wrap: break-word;
+}
+.ticket table.voucher th.item, .ticket table.voucher td.item {
+  width: 75px;
+  font-weight: 600;
+}
+.ticket table.voucher th.sep, .ticket table.voucher td.sep {
+  width: 5px;
+  font-weight: 600;
+}
+.ticket table.voucher th.value, .ticket table.voucher td.value {
+  width: 100px;
+}
+.ticket .msg-1 {
+  font-size: 10px;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 3px;
+}
+.ticket .msg-2 {
+  font-size: 9px;
+  text-align: center;
+  line-height: 1;
+  margin-bottom: 2px;
+}
+.ticket .msg-3 {
+  margin: 10px 0 4px 0;
+  font-size: 9px;
+  font-weight: 600;
+  text-align: center;
+}
+.ticket .msg-4 {
+  font-size: 9px;
+  text-align: justify;
+}     
+</style></head><body onload="window.print();window.close()"><div class="ticket">${printContents}</div></body></html>`
     );
     popupWin.document.close();
   }
